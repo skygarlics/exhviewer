a0cdea8853b0fde065424506819116904a3f4ff0
MANL <skygarlics@users.noreply.github.com>
2025-03-18 09:13:14 +0900

    switching to bootstrap5

--- exh_viewer.user.js
+++ exh_viewer.user.js
@@ -7,8 +7,6 @@
 // @include       https://exhentai.org/s/*
 // @include       https://e-hentai.org/s/*
 // @require       https://code.jquery.com/jquery-3.2.1.min.js
-// @require       https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
-// @resource      bt https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @grant         GM_deleteValue
@@ -154,12 +152,11 @@ class EXHaustViewer {
         iframe.style.zIndex = '9999';
         iframe.style.display = 'none';
 
-        // inje iframe html
+        // injet iframe html
         iframe.srcdoc = '<!DOCTYPE html><html>' +
             '<head>' +
-                '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>' +
-                '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>' +
-                '<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">' +
+                '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">' +
+                '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>' +
                 // custom css
                 '<style>' +
                     this.viewer_style +
